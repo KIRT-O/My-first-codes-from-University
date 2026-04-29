326a5a4b9a31df9d088ae41f3600d68c06274aba
Kirito <basragamer789@gmail.com>
2026-04-29 21:42:03 +0300

    WIP: basic structure for substitution

---
+++ CS50_Journey/week2/problemSet2/README.md
@@ -0,0 +1,8 @@
+# \# CS50 WIP
+
+# \--------------- 
+
+This code file is under Construction for CS50 week 2 problem set 2.
+
+i will reupload the file when i finish solving the problem.   
+
---
+++ CS50_Journey/week2/problemSet2/substitution.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2 )
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    string key = argv[1];
+    if (strlen(key) != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 2;
+    }
+    printf("%s\n", key);
+}
+
