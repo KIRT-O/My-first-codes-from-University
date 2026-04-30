862a5a9a843b615a5999639dd2af0a8eb0eb5d6a
Kirito <basragamer789@gmail.com>
2026-04-30 22:16:33 +0300

    Working on the projest (still unfinished)

--- CS50_Journey/week2/problemSet2/substitution.c
+++ CS50_Journey/week2/problemSet2/substitution.c
@@ -3,19 +3,34 @@
 #include <ctype.h>
 #include <string.h>
 
+void replaceLetters(string a);
+
+// To make the user input a key
 int main(int argc, string argv[])
 {
+    // to force the user to use a key with 26 letters
     if (argc != 2 )
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
     string key = argv[1];
+    // to count the length of the letters
     if (strlen(key) != 26)
     {
         printf("Key must contain 26 characters.\n");
         return 2;
     }
-    printf("%s\n", key);
+    //getting the message we need to ecrypt
+    string userInput;
+    userInput = get_string("plaintest: ");
 }
 
+void replaceLetters(string a)
+{
+    int n = strlen(a);
+    for (int i = 0; i < n; i++)
+    {
+        char c = a[i];
+    }
+}
